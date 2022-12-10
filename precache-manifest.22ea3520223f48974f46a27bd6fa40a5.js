26312cd1ed268b330397ca326902802c2d3c8d95
Jeremy D Glebe <jeremyglebe@gmail.com>
2022-12-09 22:40:30 -0600

    adding dist subtree

---
+++ precache-manifest.22ea3520223f48974f46a27bd6fa40a5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397c9bc25dabb766a833af053292be63",
+    "url": "/Coursework-Project-Computer-Graphics/assets/ammo/ammo.js"
+  },
+  {
+    "revision": "17fbc600c912d8a1fc6693ebd4831fd2",
+    "url": "/Coursework-Project-Computer-Graphics/assets/ammo/ammo.wasm.js"
+  },
+  {
+    "revision": "8640fe8d5bf91d2e84ea56e0c1307ba9",
+    "url": "/Coursework-Project-Computer-Graphics/assets/ammo/ammo.wasm.wasm"
+  },
+  {
+    "revision": "9c5375da5ba6989534bec275bb51a426",
+    "url": "/Coursework-Project-Computer-Graphics/assets/fbx/ASSET_CREDITS.md"
+  },
+  {
+    "revision": "8add8e369b3d7e191539125b3ed3242b",
+    "url": "/Coursework-Project-Computer-Graphics/assets/fbx/Soldier.fbx"
+  },
+  {
+    "revision": "12c06b56e324f3cc3ef2f1df9aea11ec",
+    "url": "/Coursework-Project-Computer-Graphics/assets/fbx/Soldier/Walk.fbx"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "/Coursework-Project-Computer-Graphics/assets/img/phaser-logo.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "/Coursework-Project-Computer-Graphics/favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "/Coursework-Project-Computer-Graphics/icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "/Coursework-Project-Computer-Graphics/icons/icons-512.png"
+  },
+  {
+    "revision": "c9aef2276ca78e0d4ca50b66632226fc",
+    "url": "/Coursework-Project-Computer-Graphics/index.html"
+  },
+  {
+    "revision": "0a729c8a97d30e9bc3ab",
+    "url": "/Coursework-Project-Computer-Graphics/main.d9fa027e9e2f38629bb6.bundle.js"
+  },
+  {
+    "revision": "ff0d08eca92dee1c2fc3a94297f58aeb",
+    "url": "/Coursework-Project-Computer-Graphics/manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/Coursework-Project-Computer-Graphics/sw.js"
+  },
+  {
+    "revision": "3055f8ba2a024a10fa34",
+    "url": "/Coursework-Project-Computer-Graphics/vendors.36f645d368c6f7837985.bundle.js"
+  }
+]);
